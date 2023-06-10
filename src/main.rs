08e14ccf7c9feac0f9a9c7cb0c49953353851ec8
Bartek O <bartek@fos.nl>
2023-06-10 18:12:07 +0200

    Moved setup system to systems module
    so all systems are in the same place

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use bevy::prelude::*;
 
-mod setup;
 mod resources;
 mod systems;
 
@@ -10,7 +9,6 @@ async fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(resources::ResourcesPlugin{})
-        .add_startup_system(setup::setup)
         .add_plugin(systems::SystemsPlugin{})
         .run();
 }
--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::{ Plugin, App };
 
+mod setup;
 mod size_camera;
 
 
@@ -7,6 +8,7 @@ pub struct SystemsPlugin {}
 impl Plugin for SystemsPlugin {
     fn build(&self, app: &mut App) {
         app
+            .add_startup_system(setup::setup)
             .add_system(size_camera::size_camera);
     }
 }
